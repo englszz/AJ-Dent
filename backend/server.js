a8b82699ec16262748b1765d159b3958c51089f7
Engels Smith <engelsdamiron9@gmail.com>
2026-05-13 21:26:04 -0400

    Fix: backend agregado como carpeta normal (removido submódulo)

---
+++ backend/server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+console.log(process.env.GROQ_API_KEY);
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/api/chat', async (req, res) => {
+
+    try {
+
+        console.log('BODY RECIBIDO:', req.body);
+
+        const response = await fetch(
+            'https://api.groq.com/openai/v1/chat/completions',
+            {
+                method: 'POST',
+
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${process.env.GROQ_API_KEY}`
+                },
+
+                body: JSON.stringify({
+                    model: 'llama-3.1-8b-instant',
+                    max_tokens: 300,
+                    messages: req.body.messages
+                })
+            }
+        );
+
+        const data = await response.json();
+
+        console.log('RESPUESTA GROQ:', data);
+
+        res.json(data);
+
+    } catch (error) {
+
+        console.error('ERROR:', error);
+
+               res.status(500).json({
+            error: 'Error del servidor'
+        });
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Servidor corriendo en puerto 3000');
+});
